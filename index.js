3fd9271e5512a5d6283cd6385e52577d098c36fd
Aelitta Ezugbaya <aelittae@gmail.com>
2018-01-15 14:07:08 +0200

    added form which centered on typed position

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function initMap() {
         zoom: zoomV
     });
 }
+
+var form = document.getElementById('form1');
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': document.getElementById('text').value}, function(results,status){
+        if(status=='OK'){
+            map.setCenter(results[0].geometry.location);
+        }else{
+            console.error('some mistake')
+        }
+    })
+})
