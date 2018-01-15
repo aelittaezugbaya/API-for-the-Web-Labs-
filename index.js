57f405dd1dd9cb81f72756f4aa43c42fb9e1f37f
Aelitta Ezugbaya <aelittae@gmail.com>
2018-01-15 14:36:01 +0200

    placed input in the center

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let map;
 let centerV ;
-let zoomV = 8;
+let zoomV = 11;
 function initMap() {
     centerV = new google.maps.LatLng(-34.397,150.644);
     map = new google.maps.Map(document.getElementById('map'), {
