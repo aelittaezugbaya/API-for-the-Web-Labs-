ea7952c7d8425c430bb1ad2eb0f51055d22d0529
Aelitta Ezugbaya <aelittae@gmail.com>
2018-01-16 09:18:38 +0200

    added lint to my project

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 let map;
-let centerV ;
-let zoomV = 11;
+let centerV;
+const zoomV = 11;
 function initMap() {
-    centerV = new google.maps.LatLng(-34.397,150.644);
+    centerV = new google.maps.LatLng(-34.397, 150.644);
     map = new google.maps.Map(document.getElementById('map'), {
-        center: centerV ,
-        zoom: zoomV
+        center: centerV,
+        zoom: zoomV,
     });
 }
 
-var form = document.getElementById('form1');
-form.addEventListener('submit',function(event){
+const form = document.getElementById('form1');
+form.addEventListener('submit', (event)=>{
     event.preventDefault();
-    let geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'address': document.getElementById('text').value}, function(results,status){
-        if(status=='OK'){
-            map.setCenter(results[0].geometry.location);
-        }else{
-            console.error('some mistake')
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': document.getElementById('text').value},
+        (results, status)=> {
+            if (status=='OK') {
+                map.setCenter(results[0].geometry.location);
+            } else {
+                console.error('some mistake');
+            }
         }
-    })
-})
+    );
+});
