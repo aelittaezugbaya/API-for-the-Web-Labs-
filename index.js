69642dde77fc483b3c1e38bd283776e5bb375320
Aelitta Ezugbaya <aelittae@gmail.com>
2018-01-29 21:44:57 +0200

    added markers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /* global google */
+/* global $ */
 /* eslint no-unused-vars: "off" */
 let map;
 let centerV;
-const zoomV = 11;
+const zoomV = 14;
+let lat = -34.397;
+let lng = 150.644;
+let markers = [];
 function initMap() {
-    centerV = new google.maps.LatLng(-34.397, 150.644);
+    centerV = new google.maps.LatLng(lat, lng);
     map = new google.maps.Map(document.getElementById('map'), {
         center: centerV,
         zoom: zoomV,
@@ -19,9 +23,39 @@ form.addEventListener('submit', (event)=>{
         (results, status)=>{
             if (status=='OK') {
                 map.setCenter(results[0].geometry.location);
+                console.log(map.getCenter().lat());
+                lat = map.getCenter().lat();
+                lng = map.getCenter().lng();
             } else {
                 console.error('some mistake');
             }
         }
     );
+});
+
+const wiki = document.getElementById('wikipedia');
+wiki.addEventListener('click', () => {
+    markers.forEach((marker) => {
+        marker.setMap(null);
+    });
+    markers.length = 0;
+    $.ajax({
+        url: 'http://api.geonames.org/findNearbyWikipediaJSON',
+        dataType: 'json',
+        data: {
+            lat: lat,
+            lng: lng,
+            username: 'aelittae',
+        },
+      }).done(function(data) {
+        console.log(data);
+        data.geonames.forEach((place) => {
+            const marker = new google.maps.Marker({
+                    position: {lat: place.lat, lng: place.lng},
+                    map: map,
+                    title: place.summary,
+                  });
+            markers.push(marker);
+        });
+      });
 });
